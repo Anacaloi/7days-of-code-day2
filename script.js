13923979d543a28f93b5def54b0145efa0eaa909
Anacaloi <devanacaloi@gmail.com>
2022-03-26 14:23:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function concluido() {
   var nome = document.getElementById("nome").value;
   var idade = document.getElementById("idade").value;
   var linguagem = document.getElementById("linguagem").value;
-  
-let mensagemBoasVindas = document.getElementById("boasVindas");
- 
-  mensagemBoasVindas.innerHTML = "Olá " + nome + " você tem " + idade + " anos e já está estudando " + linguagem + "!";}
+  var boasVindas = `Olá ${nome} você tem ${idade} anos e já está estudando ${linguagem}!`;
+
+  document.getElementById("boasVindas").innerHTML = boasVindas;
+}
